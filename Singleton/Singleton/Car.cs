52a419e91525fe5b3226962cb0f9d4e625178eb6
gkwlsdn17 <gkwlsdn17@gmail.com>
2021-12-17 16:47:30 +0900

    singleton

---
+++ Singleton/Singleton/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class Car
+    {
+        private static Car car;
+        private static int carNum = 111111;
+
+        public static Car getInstance()
+        {
+            if (car == null)
+            {
+                car = new Car();
+            }
+            return car;
+        }
+
+        public void setCarNum(int num)
+        {
+            carNum = num;
+        }
+
+        public int getCarNum()
+        {
+            return carNum;
+        }
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = Car.getInstance();
+            Car car2 = Car.getInstance();
+
+            unsafe
+            {
+                TypedReference tr1 = __makeref(car1);
+                TypedReference tr2 = __makeref(car2);
+                IntPtr pt1 = **(IntPtr**)(&tr1);
+                IntPtr pt2 = **(IntPtr**)(&tr2);
+                Console.WriteLine($"car1 address: {pt1}");
+                Console.WriteLine($"car2 address: {pt2}");
+            }
+            
+
+            Console.WriteLine("car1 carNum:" + car1.getCarNum());
+            Console.WriteLine("car2 carNum:" + car2.getCarNum());
+
+            car1.setCarNum(111122);
+            Console.WriteLine("=================================");
+            Console.WriteLine("car1 carNum:" + car1.getCarNum());
+            Console.WriteLine("car2 carNum:" + car2.getCarNum());
+
+            car2.setCarNum(375614);
+            Console.WriteLine("=================================");
+            Console.WriteLine("car1 carNum:" + car1.getCarNum());
+            Console.WriteLine("car2 carNum:" + car2.getCarNum());
+        }
+    }
+}
